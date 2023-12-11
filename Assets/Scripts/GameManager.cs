7993d3d54d041b38f3642b826e7e04b232f6620d
Kaan Demir <sadecekaandemir@gmail.com>
2023-12-11 14:57:43 +0300

    save

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,18 +20,33 @@ public class GameManager : MonoBehaviour
     public UnityAction onGameFinish;
 
     [SerializeField] private BallMovement m_Ball;
-    [SerializeField] private GameObject m_PanelsPArent;
+    [SerializeField] private GameObject m_PanelsParent;
+    [SerializeField] private GameObject m_EndGamePanel;
+    [SerializeField] private GameObject m_StartGamePanel;
 
     [SerializeField] private Vector2 m_ArenaLimit;
     public Vector2 GetArenaLimit => m_ArenaLimit;
 
-    private void Start()
+    private void Awake()
     {
-        onGameFinish += OpenPanelsParent;
+        onGameStart += SetOnGameStartPanels;
+        onGameFinish += SetOnGameFinishPanels;
+    }
+
+    public void StartGame()
+    {
         onGameStart?.Invoke();
     }
 
-    private void OpenPanelsParent()
-        => m_PanelsPArent.SetActive(true);
+    private void SetOnGameFinishPanels()
+    {
+        m_PanelsParent.SetActive(true);
+        m_EndGamePanel.SetActive(true);
+    }
 
+    private void SetOnGameStartPanels()
+    {
+        m_StartGamePanel.SetActive(false);
+        m_PanelsParent.SetActive(false);
+    }
 }
